5e20bf5f91617db639a6df533d25bc3f5b889e25
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2026-09-23 12:04:15 +0200

    chore(thesis): regenerate snippets and facts for the new notation

--- thesis/shared/generated/snippets/sound_dg_spec_core.thy
+++ thesis/shared/generated/snippets/sound_dg_spec_core.thy
@@ -2,20 +2,26 @@
 locale sound_dg_spec_core =
   fixes S :: "('x,'k,unit,'D::bounded_semilattice_sup_bot,
                 'G::bounded_semilattice_sup_bot) dg_spec"
-    and gammaDG :: "'D \<Rightarrow> 'G \<Rightarrow> store set"
-    and gs :: "vname \<Rightarrow> bool"
+    and \<gamma>\<^sub>D\<^sub>G :: "'D \<Rightarrow> 'G \<Rightarrow> store set"
+    and \<G> :: "vname \<Rightarrow> bool"
   assumes spec_wf: "dg_spec_wf S"
     and gammaDG_mono:
-      "\<lbrakk>d \<le> d'; g \<le> g'\<rbrakk> \<Longrightarrow> gammaDG d g \<subseteq> gammaDG d' g'"
+      "\<lbrakk>d \<le> d'; g \<le> g'\<rbrakk> \<Longrightarrow> \<gamma>\<^sub>D\<^sub>G d g \<subseteq> \<gamma>\<^sub>D\<^sub>G d' g'"
     and step_sound:
-      "edge_collect a (gammaDG (locals (\<tau> src)) (globs (\<tau> (Inr gk))))
-         \<subseteq> gammaDG (locals (traverse_program (dg_spec_edge_program S a src (\<lambda>_. gk)) \<tau>))
-                   (globs (sides_of_program (dg_spec_edge_program S a src (\<lambda>_. gk)) \<tau> (Inr gk)))"
+      "edge_collect a (\<gamma>\<^sub>D\<^sub>G (locals (\<tau> src)) (globs (\<tau> (Inr gk))))
+         \<subseteq> \<gamma>\<^sub>D\<^sub>G
+           (locals (traverse_program
+              (dg_spec_edge_program S a src (\<lambda>_. gk)) \<tau>))
+           (globs (sides_of_program
+              (dg_spec_edge_program S a src (\<lambda>_. gk)) \<tau> (Inr gk)))"
     and combine_sound:
-      "\<lbrakk>s \<in> gammaDG dc (globs (\<tau> (Inr gk)));
-        t \<in> gammaDG de (globs (\<tau> (Inr gk)))\<rbrakk> \<Longrightarrow>
-        combine_collect gs (ci_dst ci) s t
-          \<in> gammaDG (locals (traverse_rhs (sp_compile_with (\<lambda>d. DG d bot)
-                  (dg_spec_combine_transfer S ci (mk_dg_man dc (\<lambda>_. gk)) de)) \<tau>))
-                    (globs (sides_of_rhs (sp_compile_with (\<lambda>d. DG d bot)
-                  (dg_spec_combine_transfer S ci (mk_dg_man dc (\<lambda>_. gk)) de)) \<tau> (Inr gk)))"
+      "\<lbrakk>s \<in> \<gamma>\<^sub>D\<^sub>G dc (globs (\<tau> (Inr gk)));
+        t \<in> \<gamma>\<^sub>D\<^sub>G de (globs (\<tau> (Inr gk)))\<rbrakk> \<Longrightarrow>
+        combine_collect \<G> (ci_dst ci) s t
+          \<in> \<gamma>\<^sub>D\<^sub>G
+            (locals (traverse_rhs (sp_compile_with (\<lambda>d. DG d \<bottom>)
+               (dg_spec_combine_transfer S ci (mk_dg_man dc (\<lambda>_. gk)) de))
+               \<tau>))
+            (globs (sides_of_rhs (sp_compile_with (\<lambda>d. DG d \<bottom>)
+               (dg_spec_combine_transfer S ci (mk_dg_man dc (\<lambda>_. gk)) de))
+               \<tau> (Inr gk)))"
--- thesis/shared/generated/snippets/sound_domain.thy
+++ thesis/shared/generated/snippets/sound_domain.thy
@@ -1,8 +1,7 @@
 (* src/Abstract_Interpreter/Domain/Abstract_Domain.thy *)
 class sound_domain = executable_domain +
-  fixes gamma :: "'a \<Rightarrow> int set"
-  assumes gamma_bot[simp]: "gamma bot = {}"
-  assumes gamma_top[simp]: "gamma top = UNIV"
-  assumes gamma_mono: "a \<le> b \<Longrightarrow> gamma a \<subseteq> gamma b"
-  assumes is_empty_correct: "is_empty a \<longleftrightarrow> gamma a = {}"
-  assumes is_full_correct: "is_full a \<longleftrightarrow> gamma a = UNIV"
+  fixes gamma :: "'a \<Rightarrow> int set" ("\<gamma>")
+  assumes gamma_bot[simp]: "\<gamma> \<bottom> = {}"
+  assumes gamma_top[simp]: "\<gamma> \<top> = UNIV"
+  assumes gamma_mono: "a \<le> b \<Longrightarrow> \<gamma> a \<subseteq> \<gamma> b"
+  assumes is_empty_correct: "is_empty a \<longleftrightarrow> \<gamma> a = {}"
